8620f3880a40b6ef90963d495ef0f871ea28ebe0
Seavanas <sw92410@hotmail.com>
2017-09-26 19:49:16 -0600

    Added perlin, first wave
    Problem: sometimes enemy1 moves very slow

--- ProjectToufu/Assets/Level Scene/Script/Enemy Script/Enemy1Script.cs
+++ ProjectToufu/Assets/Level Scene/Script/Enemy Script/Enemy1Script.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy1Script : MonoBehaviour {
+    private BaseEnemyScript baseEnemyScript;
+    private bool triggerOnceOnly = false;
+
+    private Rigidbody2D rb;
+    private Vector2 perlinSeed;
+    public Vector2 perlinRate;
+    public float speed;
 
     // Use this for initialization
     void Start()
     {
         baseEnemyScript = GetComponent<BaseEnemyScript>();
+        rb = GetComponent<Rigidbody2D>();
+        perlinSeed = new Vector2(Random.Range(1, 10), Random.Range(1, 10));
     }
-    private BaseEnemyScript baseEnemyScript;
-    private bool triggerOnceOnly = false;
 
 	// Update is called once per frame
 	void Update () {
@@ -22,8 +29,12 @@ public class Enemy1Script : MonoBehaviour {
                 baseEnemyScript.scoreAdd();
                 triggerOnceOnly = true;
             }
-            
-        }
-            
+        } 
 	}
+
+    void FixedUpdate()
+    {
+        float perlineValue = Mathf.PerlinNoise(perlinSeed.x + Time.time * perlinRate.x, perlinSeed.y + Time.time * perlinRate.y);
+        rb.velocity = transform.up * speed * perlineValue;
+    }
 }
