04e6ca3589989b67701ce9fecb6df9395428f03c
Seavanas <sw92410@hotmail.com>
2017-12-27 15:28:32 -0700

    Improve Spike boss script, update hitxboxes, and prefab
    Use a forces to perform charge attack, ensure charge attack is behave as
    expected (charging at player's old position, stops when hit border or
    player, not changing direction during charge)

--- ProjectToufu/Assets/Level Scene/Script/Enemy Script/ChargeScan.cs
+++ ProjectToufu/Assets/Level Scene/Script/Enemy Script/ChargeScan.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ChargeScan : MonoBehaviour {
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)      //On enter won't work, will sometime stop charging 
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !GetComponentInParent<SpikeShield>().chargeStart)      //Avoid changing direction when the charge attack is in progress
         {
+            GetComponentInParent<SpikeShield>().oldSelfPosition = GetComponentInParent<SpikeShield>().transform.position;
             GetComponentInParent<SpikeShield>().chargePosition = other.transform.position;  //Set the position to charge, use transform will use reference instead of values
-            GetComponentInParent<SpikeShield>().targetFound = true;         //Begin charging
+            GetComponentInParent<SpikeShield>().chargeStart = true;         //Begin charging
         }
     }
 }
--- ProjectToufu/Assets/Level Scene/Script/Enemy Script/SpikeShield.cs
+++ ProjectToufu/Assets/Level Scene/Script/Enemy Script/SpikeShield.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class SpikeShield : MonoBehaviour
 {
     //Attack
-    public Vector2 chargePosition;
-    public bool targetFound;
+    public Vector3 oldSelfPosition;
+    public Vector3 chargePosition;
+    public bool chargeStart;
     public float chargeRate;
     private float nextCharge;
-    private bool charging;
+    private bool chargeStop;
 
     //Movement stuff
     public float chargeSpeed;
+    public float turnSpeed;
     private Rigidbody2D rb;
     //private GameObject target;
 
@@ -20,6 +22,7 @@ public class SpikeShield : MonoBehaviour
     public BaseEnemyScript baseEnemyScript;
     public BaseBossScript baseBossScript;
     private float currentHealth;
+    private bool enrage;
 
     void Start ()
     {
@@ -34,32 +37,58 @@ public class SpikeShield : MonoBehaviour
             baseBossScript.SetHealth(baseEnemyScript.health / baseEnemyScript.GetMaxHealth());
             currentHealth = baseEnemyScript.health;
         }
-        else
+
+        //Experimenting
+        if (currentHealth == 450)
         {
-            GameObject target = GameObject.FindGameObjectWithTag("Player");
-            if (target != null)
+            enrage = true;
+            chargeRate = 0.5f;
+            chargeSpeed = 20;
+            turnSpeed = 5;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+        if (target != null)
+        {
+            if ((Time.time > nextCharge) && chargeStart)     //Start charging or in the middle of charging
             {
-                if ((Time.time > nextCharge) && targetFound || charging)     //Start charging or in the middle of charging
+                TurnToTarget(oldSelfPosition, chargePosition);      //Make sure to charge at "about" the chargePosition without turning back when charge past the target position
+
+                if (Vector2.Angle(-transform.up, chargePosition - oldSelfPosition) < 1)  //make sure only charge when facing at "about" the chargePosition 
+                    rb.velocity = -transform.up * chargeSpeed;  //Charge forward
+
+                //transform.position += (chargePosition - transform.position).normalized * chargeSpeed * Time.deltaTime;
+                //transform.position = Vector2.MoveTowards(transform.position, chargePosition, chargeSpeed);  //Charge towards chargePosition
+                if (chargeStop)      //Finish charging (hit the border or the player)
                 {
-                    charging = true;    //Charging atm
-                    transform.position = Vector2.MoveTowards(transform.position, chargePosition, chargeSpeed);  //Charge towards chargePosition
-                    if ((Vector2)transform.position == chargePosition)      //Reached the chargePosition
-                    {
-                        charging = false;   //Not charging anymore
-                        nextCharge = Time.time + chargeRate;   //Timer starts ticking once the charge is over (meaning that it starts counting once the enemy is at the bottom)
-                    }
+                    chargeStart = false;
+                    chargeStop = false;
+                    rb.velocity = Vector2.zero;
+                    nextCharge = Time.time + chargeRate;   //Charge attack goes on cold down
                 }
-                else
-                {
-                    TurnToTarget(target);   //Try to face the target
-                }
+            }
+            else
+            {
+                TurnToTarget(transform.position, target.transform.position);   //Try to face the target
             }
         }
     }
 
-    void TurnToTarget(GameObject target)
+    void TurnToTarget(Vector3 self, Vector3 target)
     {
-        float angleDifference = Vector2.SignedAngle(-transform.up, target.transform.position - transform.position);      //Closest angle to target, -transform because the init sprite
-        transform.Rotate(0, 0, angleDifference * 2 * Time.deltaTime);   //Turn 2*angle per sec, so the larger the angle the faster it turns
+        float angleDifference = Vector2.SignedAngle(-transform.up, target - self);      //Closest angle to target, -transform because the init sprite
+        transform.Rotate(0, 0, angleDifference * turnSpeed * Time.deltaTime);   //Turn turnSpeed*angle per sec, so the larger the angle the faster it turns
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Border") || other.CompareTag("Player"))   //To prevent player goes into border and let boss charge off
+        {
+            chargeStop = true;
+        }
+    }
+    //Sometime when player quickly moves in and out of the ChargeScan hitbox boss won't attack, might be the update rate too slow
 }
