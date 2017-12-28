e70b0e562d7522109e0bf9f680b2376885f964e5
AT41 <anthony.tang2@ucalgary.ca>
2017-12-28 01:26:59 -0700

    Forgot a script

---
+++ ProjectToufu/Assets/UpgradeMenuScene/Scripts/TabScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabScript : MonoBehaviour {
+    public GameObject othertab1, othertab2;
+    public GameObject tabPage, otherTabPage1, otherTabPage2;
+    public Color setOtherTabColor;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonPressed()
+    {
+        GetComponent<Image>().color = new Color(255,255,255,255);
+        othertab1.GetComponent<Image>().color = setOtherTabColor;
+        othertab2.GetComponent<Image>().color = setOtherTabColor;
+        tabPage.SetActive(true);
+        otherTabPage1.SetActive(false);
+        otherTabPage2.SetActive(false);
+
+    }
+}
