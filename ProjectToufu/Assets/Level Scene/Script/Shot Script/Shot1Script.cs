1b4864b6a9fbe011a3a239cce90ae791b57bcd45
AT41 <anthony.tang2@ucalgary.ca>
2017-12-30 21:57:13 -0700

    Implemented some upgrades

--- ProjectToufu/Assets/Level Scene/Script/Shot Script/Shot1Script.cs
+++ ProjectToufu/Assets/Level Scene/Script/Shot Script/Shot1Script.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Shot1Script : MonoBehaviour
 {
+    void Start()//Sets damage equal to the save object
+    {
+        this.GetComponent<InjuresEnemy>().damage = SaveInfo.saveInfo.SaveObject.MainAttackStrength;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
             GetComponent<Mover>().SetSpeed(0);
     }
 
-
+   
 }
---
+++ ProjectToufu/Assets/UpgradeMenuScene/Scripts/UpgradePanelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePanelScript : MonoBehaviour {
+    private SaveInfoObject obj;
+    void Start()
+    {
+        obj = SaveInfo.saveInfo.SaveObject;
+    }
+    public void IncreaseFrequency()
+    {
+        obj.AttackFrequency /= 1.5f;
+    }
+    public void IncreaseHealth()
+    {
+        obj.MaxHealth += 200;
+    }   
+    public void IncreaseDamage()
+    {
+        obj.MainAttackStrength += 5;
+    }
+}
