0f7d3698fc68d4a79bc100482a95cbb1ef4d1244
AT41 <anthony.tang2@ucalgary.ca>
2018-01-05 13:15:37 -0700

    Worked on UpgradeMenu

---
+++ ProjectToufu/Assets/Resources/UpgradeMenuScene/Scripts/UIRingScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRingScript : MonoBehaviour {
+    public float speed;
+    public float rotationZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+        rotationZ += speed;
+        rotationZ = rotationZ % 360;
+	}
+}
